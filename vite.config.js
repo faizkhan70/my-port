0f6ac9f30c0f18068de734b66c002c35c1d421ca
faizkhan70 <fk3574533@gmail.com>
2025-12-25 15:30:28 +0530

    set

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,13 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/",          // 👈 keep base here
+  base: "/", // ✅ correct for Netlify
   plugins: [
     react(),
     tailwindcss()
   ],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
 })
